ccf2227c583ae939617900293286f02e2e9c6205
minhluan1998 <minhluan1998@gmail.com>
2019-03-08 00:35:56 +0700

    Update ChoKhanh.cs

--- SignalRChat/DAO/ChoKhanh.cs
+++ SignalRChat/DAO/ChoKhanh.cs
@@ -7,7 +7,18 @@ namespace SignalRChat.DAO
 {
     public class ChoKhanh
     {
+<<<<<<< HEAD
 
+=======
+        public int MyProperty { get; set; }
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+>>>>>>> parent of e2d184b... Delete ChoKhanh.cs
 
     }
 }
